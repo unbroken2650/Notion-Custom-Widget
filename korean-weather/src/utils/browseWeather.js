94b5da69e31bcd7dfc2a4debc80a2040e5e1c614
UnbroKen <unbroken2650@gmail.com>
2022-08-22 21:02:36 +0900

    :triangular_flag_on_post: Weather API
    Completed getting current weather data

---
+++ korean-weather/src/utils/browseWeather.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+const getUltraSrtNcst =
+  "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst";
+
+function browserWeather(x, y) {
+  const url = `${getUltraSrtNcst}?serviceKey=${process.env.REACT_APP_API_KEY}&numOfRows=10&pageNo=1
+  &dataType=JSON&base_date=20220822&base_time=0600&nx=${x}&ny=${y}
+  `;
+  const currWeather = {
+    T1H: 0,
+    RN1: 0,
+    UUU: 0,
+    VVV: 0,
+    REH: 0,
+    PTY: 0,
+    VEC: 0,
+    WSD: 0,
+  };
+
+  axios
+    .get(url)
+    .then((response) => response.data.response.body.items.item)
+    .then((data) => {
+      data.forEach((w) => {
+        if (Object.keys(currWeather).includes(w.category)) {
+          currWeather[w.category] = w.obsrValue;
+        }
+      });
+      console.log(currWeather);
+    });
+  return currWeather;
+}
+browserWeather(59, 123);
+export default browserWeather;
